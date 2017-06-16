4d30e08b761c2a0c9e98f0371ed4fcb242cbd056
陈斌 <chenbin05@maoyan.com>
2017-06-16 15:06:46 +0800

    WNSafeMutableArray

---
+++ CBStudyProject/CBStudyProject/Foundtion+Addition/WNSafeMutableArray.h
@@ -0,0 +1,13 @@
+//
+//  WNSafeMutableArray.h
+//  CBStudyProject
+//
+//  Created by ChenBin on 2017/6/16.
+//  Copyright © 2017年 ChenBin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WNSafeMutableArray : NSMutableArray
+
+@end
---
+++ CBStudyProject/CBStudyProject/Foundtion+Addition/WNSafeMutableArray.m
@@ -0,0 +1,17 @@
+//
+//  WNSafeMutableArray.m
+//  CBStudyProject
+//
+//  Created by ChenBin on 2017/6/16.
+//  Copyright © 2017年 ChenBin. All rights reserved.
+//
+
+#import "WNSafeMutableArray.h"
+
+@implementation WNSafeMutableArray
+
++ (void)load {
+    
+}
+
+@end
